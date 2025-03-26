590c3e2df978602fd0404d3c352267e3cecb3fd7
feb101996 <jaudianclepher@yahoo.com>
2025-03-26 01:51:09 +0000

    Change UI - handling edge cases

--- dist/sources/decomp.js
+++ dist/sources/decomp.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.decomp = exports.Eval_decomp = void 0;
-const defs_1 = require("../runtime/defs");
-const find_1 = require("../runtime/find");
-const stack_1 = require("../runtime/stack");
-const misc_1 = require("../sources/misc");
-const add_1 = require("./add");
-const eval_1 = require("./eval");
-const guess_1 = require("./guess");
-const list_1 = require("./list");
-const multiply_1 = require("./multiply");
+var defs_1 = require("../runtime/defs");
+var find_1 = require("../runtime/find");
+var stack_1 = require("../runtime/stack");
+var misc_1 = require("../sources/misc");
+var add_1 = require("./add");
+var eval_1 = require("./eval");
+var guess_1 = require("./guess");
+var list_1 = require("./list");
+var multiply_1 = require("./multiply");
 // this function extract parts subtrees from a tree.
 // It is used in two
 // places that have to do with pattern matching.
@@ -24,10 +24,10 @@ const multiply_1 = require("./multiply");
 // be evalled. Never called.
 function Eval_decomp(p1) {
     console.log('Eval_decomp is being called!!!!!!!!!!!!!!!!!!!!');
-    const arg = eval_1.Eval(defs_1.cadr(p1));
+    var arg = eval_1.Eval(defs_1.cadr(p1));
     p1 = eval_1.Eval(defs_1.caddr(p1));
-    const variable = p1 === defs_1.symbol(defs_1.NIL) ? guess_1.guess(arg) : p1;
-    const result = decomp(false, arg, variable);
+    var variable = p1 === defs_1.symbol(defs_1.NIL) ? guess_1.guess(arg) : p1;
+    var result = decomp(false, arg, variable);
     stack_1.push(list_1.makeList(defs_1.symbol(defs_1.NIL), ...result));
 }
 exports.Eval_decomp = Eval_decomp;
@@ -74,7 +74,7 @@ function decomp(generalTransform, p1, p2) {
         console.log(' naive decomp');
         console.log(`startig p3: ${p3}`);
     }
-    const stack = [];
+    var stack = [];
     while (defs_1.iscons(p3)) {
         // for a general transformations,
         // we want to match any part of the tree so
@@ -100,7 +100,7 @@ function decomp_sum(generalTransform, p1, p2) {
     }
     // decomp terms involving x
     let p3 = defs_1.cdr(p1);
-    const stack = [];
+    var stack = [];
     while (defs_1.iscons(p3)) {
         if (find_1.Find(defs_1.car(p3), p2) || generalTransform) {
             stack.push(...decomp(generalTransform, defs_1.car(p3), p2));
@@ -109,7 +109,7 @@ function decomp_sum(generalTransform, p1, p2) {
     }
     // add together all constant terms
     p3 = defs_1.cdr(p1);
-    const constantTerms = [...p3].filter((t) => !find_1.Find(t, p2));
+    var constantTerms = [...p3].filter((t) => !find_1.Find(t, p2));
     if (constantTerms.length) {
         p3 = add_1.add_all(constantTerms);
         pushTryNotToDuplicateLocal(stack, p3);
@@ -123,7 +123,7 @@ function decomp_product(generalTransform, p1, p2) {
     }
     // decomp factors involving x
     let p3 = defs_1.cdr(p1);
-    const stack = [];
+    var stack = [];
     while (defs_1.iscons(p3)) {
         if (find_1.Find(defs_1.car(p3), p2) || generalTransform) {
             stack.push(...decomp(generalTransform, defs_1.car(p3), p2));
@@ -132,9 +132,9 @@ function decomp_product(generalTransform, p1, p2) {
     }
     // multiply together all constant factors
     p3 = defs_1.cdr(p1);
-    const constantFactors = [];
+    var constantFactors = [];
     while (defs_1.iscons(p3)) {
-        const item = defs_1.car(p3);
+        var item = defs_1.car(p3);
         if (!find_1.Find(item, p2)) {
             if (constantFactors.length < 1 ||
                 !misc_1.equal(item, constantFactors[constantFactors.length - 1])) {
