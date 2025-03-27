6f9fce20cd8b37b791ecae1621efc48596dfeee3
feb101996 <jaudianclepher@yahoo.com>
2025-03-27 22:22:08 +0000

    Fix configuration - improving performance

--- dist/sources/adj.js
+++ dist/sources/adj.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adj = exports.Eval_adj = void 0;
-const alloc_1 = require("../runtime/alloc");
-const defs_1 = require("../runtime/defs");
-const run_1 = require("../runtime/run");
-const stack_1 = require("../runtime/stack");
-const cofactor_1 = require("./cofactor");
-const eval_1 = require("./eval");
-const tensor_1 = require("./tensor");
+var alloc_1 = require("../runtime/alloc");
+var defs_1 = require("../runtime/defs");
+var run_1 = require("../runtime/run");
+var stack_1 = require("../runtime/stack");
+var cofactor_1 = require("./cofactor");
+var eval_1 = require("./eval");
+var tensor_1 = require("./tensor");
 /* adj =====================================================================
 
 Tags
@@ -24,7 +24,7 @@ Returns the adjunct of matrix m. The inverse of m is equal to adj(m) divided by 
 
 */
 function Eval_adj(p1) {
-    const result = adj(eval_1.Eval(defs_1.cadr(p1)));
+    var result = adj(eval_1.Eval(defs_1.cadr(p1)));
     stack_1.push(result);
 }
 exports.Eval_adj = Eval_adj;
@@ -32,8 +32,8 @@ function adj(p1) {
     if (!tensor_1.is_square_matrix(p1)) {
         run_1.stop('adj: square matrix expected');
     }
-    const n = p1.tensor.dim[0];
-    const p2 = alloc_1.alloc_tensor(n * n);
+    var n = p1.tensor.dim[0];
+    var p2 = alloc_1.alloc_tensor(n * n);
     p2.tensor.ndim = 2;
     p2.tensor.dim[0] = n;
     p2.tensor.dim[1] = n;
